b294e7f2c2779fd121658a6d832775523dd5d706
Huba Dmitry <hubadmitry@gmail.com>
2024-04-09 15:39:13 +0300

    XDDDDD

--- Commands.sql
+++ Commands.sql
@@ -16,7 +16,6 @@ select * from oppeaine
 INSERT INTO oppeaine(nimetus, kestvus, algus_kuupaev, kirjeldus)
 
 VALUES (
-<<<<<<< HEAD:Commands.txt
 'Fizkultura', 200, '2024-04-9', 'Üldhariduse moodul');
 select * from oppeaine
 
@@ -26,35 +25,36 @@ CREATE DATABASE HubaDmitryTITpv22;
 --tabeli loomine
 --primaty key - дает уникальное значение
 USE HubaDmitryTITpv22;
-CREATE TABLE opetaja(
-opetajaID int Primary key identity (1,1),
+CREATE TABLE opelane(
+opelaneID int Primary key identity (1,1),
 eesnimi varchar(50), 
 perenimi varchar(50) NOT NULL,
-suniaeg date,
-kontakt varchar(50));
+isikukood varchar(50) NOT NULL,
+aadress varchar(50));
 --Kuva/ Näita tabeli
-select * from opetaja
-
+select * from opelane
 --Andmete lisamine tabeli sisse
-INSERT INTO opetaja(eesnimi, perenimi, kontakt)
+INSERT INTO opelane(eesnimi, perenimi, isikukood, aadress)
 VALUES (
-'Idk', 'Who', '12345678');
-select * from opetaja
+'Idk5', 'Who5', '55', 'yep5');
+select * from opelane
 
 UPDATE opetaja SET suniaeg='1999-10-10'
 WHERE opetajaID=1;
 
 --tabeli muutmine veergu lisamine
-ALTER TABLE oppeaine ADD opetajaID int;
+ALTER TABLE hindamine ADD oppeaineID int;
 SELECT * FROM oppeaine;
 SELECT * FROM opetaja;
-
-UPDATE oppeaine SET opetajaID=1;
+select * from opelane;
+SELECT * FROM hindamine;
+UPDATE hindamine SET opelaneID=10;
 --foreign key lisamine tabeli sisse
+insert into hindamine (opelaneID, oppeaineID, hinne, kuupaev) 
+VALUES (
+100, 100, '5', '1000-10-10');
 
-ALTER TABLE oppeaine
-ADD FOREIGN KEY (opetajaID) REFERENCES opetaja(opetajaID);
-=======
-'Matemaatka', 200, '2024-04-9', 'Üldainete moodul');
-select * from oppeaine
->>>>>>> 14f9e72e56d3c0fc4097f1e410ce1aaafdccdd62:Commands.sql
+
+
+ALTER TABLE hindamine
+ADD FOREIGN KEY (opelaneID) REFERENCES oppeaine(oppeaineID);
